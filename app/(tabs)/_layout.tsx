96664dbf5ab68fbec5f7154616ed4095ae1c27e7
Nok <nicola.cleto.job@gmail.com>
2026-02-15 14:42:11 +0100

    Rimosso  import css per esere spostato back nel layout main

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,14 @@
 import { Tabs } from 'expo-router';
-import '../../global.css';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#b92a2a', headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
-    </Tabs>
+    <SafeAreaProvider>
+      <Tabs screenOptions={{ tabBarActiveTintColor: '#b92a2a', headerShown: false }}>
+        <Tabs.Screen name="index" options={{ title: 'Home' }} />
+        <Tabs.Screen name="about" options={{ title: 'About' }} />
+      </Tabs>
+    </SafeAreaProvider>
   );
 }
