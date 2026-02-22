bfc1a1133592aaf5b6624949c9fa5902f828222f
Nok <nicola.cleto.job@gmail.com>
2026-02-22 10:36:25 +0100

    Aggiunto usestate della country e settato a lista ricette

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,18 +5,15 @@ import ListaRicette from '../../components/ListaRicette';
 import Mappamondo from 'components/Mappamondo';
 
 export default function WelcomePage() {
-
+  const [country, setCountry] = useState<string>('');
+  console.log(country);
   return (
     <View className="flex-1">
       <View className="basis-3/5">
-        <Mappamondo />
+        <Mappamondo setCountryID={setCountry} />
       </View>
       {/* Se è stato selezionato un paese, mostra la lista delle ricette per quel paese. Flex-1 = 50% pagina */}
-      <View className="flex-1">
-            {country !== '' && (
-            <ListaRicette country={country} />
-        )}
-        </View>
+      <View className="flex-1">{country !== '' && <ListaRicette country={country} />}</View>
     </View>
-    );
+  );
 }
