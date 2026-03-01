4a846f8ff9ca85403f3f2ac17718674660de17b3
Nok <nicola.cleto.job@gmail.com>
2026-03-01 10:32:19 +0100

    Rimosso value per costo e difficoltà aggiunto icone prefab, aggiunto render switch per difficoltà e costo

--- components/ricetta/InfoRicetta.tsx
+++ components/ricetta/InfoRicetta.tsx
@@ -1,30 +1,60 @@
-import { View, Image, Text } from 'react-native';
+import { View } from 'react-native';
 import InfoCard from './interfaccia/InfoCard';
 import { useRecipe } from '../../contexts/RecipeContex';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function InfoRicetta() {
   const { recipe } = useRecipe();
+  const RenderDifficulty = () => {
+    switch (recipe?.difficolta) {
+      case 'Facile':
+        return <MaterialCommunityIcons name="fire" size={30} color="green" />;
+      case 'Media':
+        return <MaterialCommunityIcons name="fire" size={30} color="orange" />;
+      case 'Difficile':
+        return <MaterialCommunityIcons name="fire" size={30} color="red" />;
+
+      default:
+        break;
+    }
+  };
+  const RenderCosto = () => {
+    switch (recipe?.costoPerPersona) {
+      case 'Basso':
+        return <MaterialIcons name="euro" size={30} color="green" />;
+      case 'Medio':
+        return <MaterialIcons name="euro" size={30} color="orange" />;
+      case 'Alto':
+        return <MaterialIcons name="euro" size={30} color="red" />;
+
+      default:
+        break;
+    }
+  };
   return (
     <View className="flex-1 flex-row justify-evenly px-2 ">
       {/* TODO creare componente del visualizzatore  */}
 
       <InfoCard
-        icon={require('../../assets/favicon.png')}
+        icon={<Ionicons name="timer" size={30} color="black" />}
         value={recipe?.tempoPreparazione}
         identifier={recipe?.tempoPreparazioneUnita}
       />
       <InfoCard
-        icon={require('../../assets/favicon.png')}
+        icon={<Ionicons name="timer" size={30} color="black" />}
         value={recipe?.tempoCottura}
         identifier={recipe?.tempoCotturaUnita}
       />
       <InfoCard
-        icon={require('../../assets/favicon.png')}
+        icon={<Fontisto name="persons" size={30} color="black" />}
         value={recipe?.porzioni}
         identifier="prz"
       />
-      <InfoCard icon={require('../../assets/favicon.png')} value={recipe?.costoPerPersona} />
-      <InfoCard icon={require('../../assets/favicon.png')} value={recipe?.difficolta} />
+      <InfoCard icon={RenderCosto()} />
+      <InfoCard icon={RenderDifficulty()} />
     </View>
   );
 }
