d8c6288c8f2e0a8dff6a5795a969b290f70ac4e6
Nok <nicola.cleto.job@gmail.com>
2026-04-20 10:10:03 +0200

    MOD: modificato componente per migliorare la visibilità nella pagina

--- components/ricetta/InfoRicetta.tsx
+++ components/ricetta/InfoRicetta.tsx
@@ -36,32 +36,29 @@ export default function InfoRicetta({ sizeIcons = 20 }: { sizeIcons?: number }) 
   };
   console.log('RECIPE INFO', recipe?.costoPerPersona);
   return (
-    <View className="flex-1 flex-col justify-evenly gap-2 px-2">
-      {/* TODO creare componente del visualizzatore  */}
-      <View className="flex-1 flex-row gap-2">
+    <View className="flex-1 flex-col gap-y-2 px-4">
+      <View className="mx-2  flex-1 flex-row justify-between">
         <InfoCard
           icon={<Ionicons name="timer" size={sizeIcons} color="#333" />}
           preText="Pronto in"
           value={recipe?.tempoPreparazione}
-          overrideW="!min-w-[140]"
           identifier={recipe?.tempoPreparazioneUnita}
         />
         <InfoCard
           icon={<Ionicons name="timer" size={sizeIcons} color="#333" />}
           preText="Cotto in"
           value={recipe?.tempoCottura}
-          overrideW="!min-w-[140]"
           identifier={recipe?.tempoCotturaUnita}
         />
       </View>
-      <View className="flex-row gap-2">
+      <View className="flex-row  gap-2">
         <InfoCard
           icon={<Fontisto name="persons" size={sizeIcons} color="#333" />}
           preText="Per"
           value={recipe?.porzioni}
           identifier="prz"
         />
-        <InfoCard icon={RenderCosto()} preText="Costo" overrideW="!max-w-[80]" />
+        <InfoCard icon={RenderCosto()} preText="Costo" />
         <InfoCard icon={RenderDifficulty()} preText="Difficoltà" />
       </View>
     </View>
