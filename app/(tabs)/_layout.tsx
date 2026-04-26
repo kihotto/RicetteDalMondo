d6623fcf3e445a3b41ea6d2963bf0842a6c81516
Nok <nicola.cleto.job@gmail.com>
2026-04-26 11:19:22 +0200

    MOD: Aggiunto personalizzazione per icone tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,43 @@
 import { Tabs } from 'expo-router';
-
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'react-native';
 
 export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
   return (
     <SafeAreaProvider>
-      <Tabs screenOptions={{ tabBarActiveTintColor: '#b92a2a', headerShown: false }}>
-        <Tabs.Screen name="index" options={{ title: 'Home' }} />
-        <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#315eda',
+          tabBarInactiveTintColor: '#1d45b4',
+          tabBarStyle: {
+            height: 50,
+            marginBottom: insets.bottom,
+            alignItems: 'center',
+            justifyContent: 'center',
+            /* backgroundColor: '#93c5fd', */
+
+            backgroundColor: '#80ff80',
+            borderBlockColor: '#000000',
+          },
+        }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="information-circle" size={24} color={color} />
+            ),
+          }}
+        />
       </Tabs>
     </SafeAreaProvider>
   );
